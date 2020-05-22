227d9e9a3e21f9fddd5b4da17152d6494041b544
xxsyam <xxsyam@gmail.com>
2020-05-22 17:48:42 +0900

    セミナーLPページ作成

---
+++ fal-c/pub/seminor03.php
@@ -0,0 +1,244 @@
+<section id="CONTENTS_TITLE" class="base_section">
+  <div class="base_wrapper">
+    <h2><span>Findアクティブラーナー主催</span><strong><span>第3回オンラインセミナー</span><span>「オンライン授業導入＜基礎研修編＞」</span></strong></h2>
+    <h3>2020年6月12日（金）、13日（土）急遽開催決定！</h3>
+    <p>＜セミナーお申し込みはページ下部記載＞</p>
+  </div>
+</section>
+
+<section id="CONTENTS_CATCH" class="base_section">
+  <div class="base_wrapper">
+    <h3>3回目にして計1,000名超が申し込んだ<br>話題のオンライセミナー追加日程公開！</h3>
+    <p>『ウェブで授業研究　Find！アクティブラーナー』は、6月12・13日全国の教育関係者を対象に、ZOOMを用いた「オンライン授業導入セミナー」を開催いたします。</p>
+    <p>5月15日実施された同セミナーでは、テレビ朝日「サンデーステーション」、NHK「首都圏ネットワーク」でも取り上げられ、全国から注目を集めました。</p>
+    <p>今回は、申し込み殺到により受付開始から約1日半で応募を締め切った5月29日、30日のセミナーと同様の内容を、急遽追加日程を設けお届けいたします。<br>ぜひ、この機会にお申し込みください。</p>
+  </div>
+</section>
+
+<section id="CONTENTS_GAIYO" class="base_section">
+  <div class="base_wrapper">
+    <h3>オンライセミナー概要</h3>
+
+    <article>
+      <h4>日時</h4>
+      <ul class="list_decimal">
+        <li>2020年6月12日（金） 17:15～18:45</li>
+        <li>2020年6月13日（土） 13:30～15:00</li>
+      </ul>
+      <p>
+        ※両日それぞれ300名限定（申し込み順）<br>
+        ※同じ内容を扱いますのでご参加可能な日時をお選びください</p>
+    </article>
+
+    <article>
+      <h4>当日のセミナー内容</h4>
+      <ul class="list_decimal">
+        <li>ご挨拶</li>
+        <li>テレビ会議システムの機能を活用した授業展開</li>
+        <li>教員が留意するポイント</li>
+        <li>生徒を能動的にする工夫</li>
+        <li>オンライン模擬授業<br>(アンケート、チャット、画面共有、ペアワーク、フリップメソッド、画面展開、ホワイトボード、ジェスチャーゲームなどを活用した授業例)</li>
+        <li>オンライン授業の構成の仕方</li>
+        <li>チャットによる質疑応答</li>
+        <li>対談 : 中島博司×寺裏誠司「オンライン授業の可能性について」</li>
+        <li>閉会の挨拶</li>
+      </ul>
+      <p>※当日は、「Zoom」を使用し、テレビ会議システムの機能も利用した「オンライン模擬授業」も実施いたします。講師と参加される全国の先生方との、双方向のやりとりを中心としたセミナーを実施予定です。</p>
+    </article>
+
+  </div>
+</section>
+
+<section id="CONTENTS_TOKUTEN" class="base_section">
+  <div class="base_wrapper">
+    <h3>参加者特典</h3>
+    <p>お申込みいただいた皆様に「オンライン授業導入・実践マニュアル」を無償でご提供いたします！</p>
+
+    <article>
+      <h4>開催方法：オンライン配信（ZOOM）</h4>
+      <p>※お申込みいただいた方へ事前にURLをお送りいたします</p>
+    </article>
+
+    <article>
+      <h4>開催方法：オンライン配信（ZOOM）</h4>
+      <ul class="list_decimal">
+        <li>ZOOMを活用したオンラインセミナー</li>
+        <li>テレビ会議システムの機能を活用した授業展開</li>
+        <li>参加型の研修スタイル</li>
+      </ul>
+    </article>
+
+    <article>
+      <h4>参加費用：無料</h4>
+    </article>
+  </div>
+</section>
+
+<section id="CONTENTS_KOUENSHA" class="base_section">
+  <div class="base_wrapper">
+    <h3>講演者紹介</h3>
+
+    <div id="KOUENSHA_WRAP">
+      <div class="pict_wrap"><img src="/uploads/files/images/pub_seminor03_01.jpg"></div>
+      <p>メイン講師：株式会社学び代表取締役　一般社団法人アクティブ・ラーニング協会理事　寺裏　誠司 氏</p>
+      <p>対談：株式会社Findアクティブラーナー参与　アクティブ・ラーニング協会代表理事　中島　博司 氏</p>
+    </div>
+
+  </div>
+</section>
+
+<section id="CONTENTS_FLOE" class="base_section">
+  <div class="base_wrapper">
+    <h3>お申込みの流れ</h3>
+
+    <ol>
+      <li>（1）以下、「お申し込み（無料会員登録）」ボタンをクリックし、無料会員へご登録いただきます。<br><span class="attention_text">※無料会員登録後、有料プランのご案内ページが表示されますが、そちらへのご登録は一切必要ございません</span></li>
+      <li>（2）ご登録後、ログインされた状態でトップページをご覧いただくと、赤字で「Find！アクティブラーナー主催「オンライン授業<基礎研修>」オンラインセミナー」と告知がございます</li>
+    </ol>
+
+    <div class="pict_wrap"><img src="/uploads/files/images/pub_seminor03_02.jpg"></div>
+
+    <p>その枠内「参加申し込みフォーム」をクリックし、必要事項をご記入の上お申し込みください。</p>
+
+  </div>
+</section>
+
+<div class="btn_regs"><a href="/member/authRegister">お申し込み（無料会員登録）</a></div>
+
+
+<style>
+.base_section{
+  padding:80px 5%;
+}
+.base_section *{
+  line-height:1.8;
+}
+.base_section:nth-of-type(2n+1){
+  background:#efefef;
+}
+
+.base_wrapper{
+  max-width:900px;
+  margin:auto;
+}
+.base_wrapper p{
+  margin-bottom:15px;
+}
+.base_wrapper h3,.base_wrapper article:not(:last-of-type){
+  margin-bottom:35px;
+}
+.base_wrapper h4{
+}
+.base_wrapper h4:before{
+  content:"■";
+  display: inline-block;
+  margin-right:3px;
+}
+
+.base_wrapper article ul.list_decimal li:before{
+  content:"・";
+  display: inline-block;
+  margin-right:3px;
+}
+
+.base_wrapper .pict_wrap{
+  margin-bottom:15px;
+}
+.base_wrapper .pict_wrap img{
+  width:100%;
+}
+
+.base_wrapper ol li{
+  margin-bottom:15px;
+}
+.attention_text{
+  color:red;
+  display: block;
+}
+
+.btn_regs{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width:100vw;
+}
+.btn_regs a{
+  max-width:340px;
+  margin:auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color:#fff !important;
+  border-radius: 100px;
+  font-size:20px;
+  font-weight:bold;
+  background:#FCB400;
+  padding:10px 40px;
+  text-align: center;
+}
+.btn_regs a:hover{
+  opacity:0.75;
+}
+@media(max-width:480px){
+  .btn_regs a{
+    margin:0 5%;
+    font-size:16px;
+    padding:10px 20px;
+  }
+}
+
+#CONTENTS_TITLE .base_wrapper{
+  text-align: center;
+}
+#CONTENTS_TITLE .base_wrapper h2,#CONTENTS_TITLE .base_wrapper h2 strong{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+#CONTENTS_TITLE .base_wrapper h2>span{
+  font-size:16px;
+}
+#CONTENTS_TITLE .base_wrapper h2 strong span:first-of-type{
+  font-size:24px;
+}
+#CONTENTS_TITLE .base_wrapper h2 strong span:last-of-type{
+  font-size:32px;
+  margin-bottom:35px;
+}
+#CONTENTS_TITLE .base_wrapper h3{
+  font-size:20px;
+  margin-bottom:15px;
+}
+
+@media(max-width:640px){
+  #CONTENTS_TITLE .base_wrapper h2 strong span:first-of-type{
+    font-size:20px;
+  }
+  #CONTENTS_TITLE .base_wrapper h2 strong span:last-of-type{
+    font-size:26px;
+  }
+  #CONTENTS_TITLE .base_wrapper h3{
+    font-size:18px;
+  }
+}
+@media(max-width:480px){
+  #CONTENTS_TITLE .base_wrapper h2 strong span:last-of-type{
+    font-size:22px;
+  }
+}
+
+#CONTENTS_CATCH h3{
+  text-align: center;
+}
+@media(max-width:640px){
+  #CONTENTS_CATCH h3{
+    font-size:20px;
+  }
+}
+@media(max-width:480px){
+  #CONTENTS_CATCH h3{
+    font-size:18px;
+  }
+}
+
+</style>
