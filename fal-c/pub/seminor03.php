d8f0fd960562c1b0dbf897a652d260d0907b411a
xxsyam <xxsyam@gmail.com>
2020-05-25 23:08:53 +0900

    デザインあて-ver1

--- fal-c/pub/seminor03.php
+++ fal-c/pub/seminor03.php
@@ -1,6 +1,6 @@
 <section id="CONTENTS_TITLE" class="base_section">
   <div class="base_wrapper">
-    <h2><span>Findアクティブラーナー主催</span><strong><span>第3回オンラインセミナー</span><span>「オンライン授業導入＜基礎研修編＞」</span></strong></h2>
+    <h2><span>Findアクティブラーナー主催</span><strong><span>第3回オンラインセミナー</span><span>オンライン授業導入＜基礎研修編＞</span></strong></h2>
     <h3>2020年6月12日（金）、13日（土）急遽開催決定！</h3>
     <p>＜セミナーお申し込みはページ下部記載＞</p>
   </div>
@@ -8,10 +8,20 @@
 
 <section id="CONTENTS_CATCH" class="base_section">
   <div class="base_wrapper">
-    <h3>3回目にして計1,000名超が申し込んだ<br>話題のオンライセミナー追加日程公開！</h3>
-    <p>『ウェブで授業研究　Find！アクティブラーナー』は、6月12・13日全国の教育関係者を対象に、ZOOMを用いた「オンライン授業導入セミナー」を開催いたします。</p>
-    <p>5月15日実施された同セミナーでは、テレビ朝日「サンデーステーション」、NHK「首都圏ネットワーク」でも取り上げられ、全国から注目を集めました。</p>
-    <p>今回は、申し込み殺到により受付開始から約1日半で応募を締め切った5月29日、30日のセミナーと同様の内容を、急遽追加日程を設けお届けいたします。<br>ぜひ、この機会にお申し込みください。</p>
+    <h3>3回目にして計<strong>1,000</strong>名超が申し込んだ<br>話題のオンライセミナー追加日程公開！</h3>
+    <div class="point_wrap">
+      <p>『ウェブで授業研究　Find！アクティブラーナー』は、6月12・13日<br>全国の教育関係者を対象に、ZOOMを用いた「オンライン授業導入セミナー」を開催いたします。</p>
+    </div>
+
+    <div id="CATCH_IMAGES">
+      <div class="pict_wrap"><img src="/uploads/files/images/seminor/03/pub_seminor03_00.jpg"></div>
+      <div class="text_wrap">
+        <p>5月15日実施された同セミナーでは、<strong>テレビ朝日「サンデーステーション」</strong><strong>NHK「首都圏ネットワーク」</strong>でも取り上げられ、全国から注目を集めました。</p>
+        <p>今回は、申し込み殺到により受付開始から約1日半で応募を締め切った5月29日、30日のセミナーと同様の内容を、急遽追加日程を設けお届けいたします。<br>ぜひ、この機会にお申し込みください。</p>
+      </div>
+    </div>
+
+    <h4>ぜひ、この機会にお申し込みください。</h4>
   </div>
 </section>
 
@@ -21,9 +31,9 @@
 
     <article>
       <h4>日時</h4>
-      <ul class="list_decimal">
-        <li>2020年6月12日（金） 17:15～18:45</li>
-        <li>2020年6月13日（土） 13:30～15:00</li>
+      <ul class="list_date">
+        <li><span>2020年</span><strong>6月12日（金） 17:15～18:45</strong></li>
+        <li><span>2020年</span><strong>6月13日（土） 13:30～15:00</strong></li>
       </ul>
       <p>
         ※両日それぞれ300名限定（申し込み順）<br>
@@ -41,9 +51,11 @@
         <li>オンライン授業の構成の仕方</li>
         <li>チャットによる質疑応答</li>
         <li>対談 : 中島博司×寺裏誠司「オンライン授業の可能性について」</li>
-        <li>閉会の挨拶</li>
       </ul>
-      <p>※当日は、「Zoom」を使用し、テレビ会議システムの機能も利用した「オンライン模擬授業」も実施いたします。講師と参加される全国の先生方との、双方向のやりとりを中心としたセミナーを実施予定です。</p>
+
+      <h4>閉会の挨拶</h4>
+      <p>※当日は、「Zoom」を使用し、テレビ会議システムの機能も利用した「オンライン模擬授業」も実施いたします。<br>
+        講師と参加される全国の先生方との、双方向のやりとりを中心としたセミナーを実施予定です。</p>
     </article>
 
   </div>
@@ -52,25 +64,29 @@
 <section id="CONTENTS_TOKUTEN" class="base_section">
   <div class="base_wrapper">
     <h3>参加者特典</h3>
-    <p>お申込みいただいた皆様に「オンライン授業導入・実践マニュアル」を無償でご提供いたします！</p>
+    <div class="point_wrap">
+      <p>お申込みいただいた皆様に「オンライン授業導入・実践マニュアル」を無償でご提供いたします！</p>
+    </div>
 
     <article>
-      <h4>開催方法：オンライン配信（ZOOM）</h4>
-      <p>※お申込みいただいた方へ事前にURLをお送りいたします</p>
+      <div class="pict_wrap"><img src="/uploads/files/images/seminor/03/pub_seminor03_01.jpg"></div>
+      <div class="text_wrap">
+        <div>
+          <h4>開催方法</h4>
+          <h5>オンライン配信（ZOOM）</h5>
+          <p>※お申込みいただいた方へ事前にURLをお送りいたします</p>
+        </div>
+
+        <div>
+          <h4>セミナー形式</h4>
+          <ul class="list_decimal">
+            <li>ZOOMを活用したオンラインセミナー</li>
+            <li>参加型の研修スタイル</li>
+          </ul>
+        </div>
+      </div>
     </article>
 
-    <article>
-      <h4>開催方法：オンライン配信（ZOOM）</h4>
-      <ul class="list_decimal">
-        <li>ZOOMを活用したオンラインセミナー</li>
-        <li>テレビ会議システムの機能を活用した授業展開</li>
-        <li>参加型の研修スタイル</li>
-      </ul>
-    </article>
-
-    <article>
-      <h4>参加費用：無料</h4>
-    </article>
   </div>
 </section>
 
@@ -79,26 +95,29 @@
     <h3>講演者紹介</h3>
 
     <div id="KOUENSHA_WRAP">
-      <div class="pict_wrap"><img src="/uploads/files/images/pub_seminor03_01.jpg"></div>
-      <p>メイン講師：株式会社学び代表取締役　一般社団法人アクティブ・ラーニング協会理事　寺裏　誠司 氏</p>
-      <p>対談：株式会社Findアクティブラーナー参与　アクティブ・ラーニング協会代表理事　中島　博司 氏</p>
+      <div class="kouensha_unit">
+        <div class="pict_wrap"><img src="/uploads/files/images/seminor/03/pub_seminor03_02.jpg"></div>
+        <p><span>株式会社学び代表取締役<br>一般社団法人アクティブ・ラーニング協会理事</span><strong>寺裏　誠司<span>氏</span></strong></p>
+      </div>
+      <div class="kouensha_unit">
+        <div class="pict_wrap"><img src="/uploads/files/images/seminor/03/pub_seminor03_03.jpg"></div>
+        <p><span>元全国高等学校長協会教育課程研究委員長<br>前茨城県立並木中等教育学校校長<br>株式会社Findアクティブラーナー参与<br>アクティブ・ラーニング協会代表理事</span><strong>中島　博司<span>氏</span></strong></p>
+      </div>
     </div>
 
   </div>
 </section>
 
-<section id="CONTENTS_FLOE" class="base_section">
+<section id="CONTENTS_FLOW" class="base_section">
   <div class="base_wrapper">
     <h3>お申込みの流れ</h3>
 
     <ol>
-      <li>（1）以下、「お申し込み（無料会員登録）」ボタンをクリックし、無料会員へご登録いただきます。<br><span class="attention_text">※無料会員登録後、有料プランのご案内ページが表示されますが、そちらへのご登録は一切必要ございません</span></li>
-      <li>（2）ご登録後、ログインされた状態でトップページをご覧いただくと、赤字で「Find！アクティブラーナー主催「オンライン授業<基礎研修>」オンラインセミナー」と告知がございます</li>
+      <li><span>Step.１</span>以下、「お申し込み（無料会員登録）」ボタンをクリックし、無料会員へご登録いただきます。<br><span class="attention_text">※無料会員登録後、有料プランのご案内ページが表示されますが、そちらへのご登録は一切必要ございません</span></li>
+      <li><span>Step.２</span>ご登録後、ログインされた状態でトップページをご覧いただくと、赤字で「Find！アクティブラーナー主催「オンライン授業<基礎研修>」オンラインセミナー」と告知がございます</li>
     </ol>
 
-    <div class="pict_wrap"><img src="/uploads/files/images/pub_seminor03_02.jpg"></div>
-
-    <p>その枠内「参加申し込みフォーム」をクリックし、必要事項をご記入の上お申し込みください。</p>
+    <div class="pict_wrap"><img src="/uploads/files/images/seminor/03/pub_seminor03_04.jpg"></div>
 
   </div>
 </section>
@@ -114,7 +133,7 @@
   line-height:1.8;
 }
 .base_section:nth-of-type(2n+1){
-  background:#efefef;
+  background:#fffce4;
 }
 
 .base_wrapper{
@@ -124,21 +143,61 @@
 .base_wrapper p{
   margin-bottom:15px;
 }
+.base_wrapper h3{
+  color:#045ac0;
+  border-left:10px solid #045ac0;
+  padding-left:15px;
+}
+
 .base_wrapper h3,.base_wrapper article:not(:last-of-type){
   margin-bottom:35px;
 }
 .base_wrapper h4{
-}
-.base_wrapper h4:before{
-  content:"■";
-  display: inline-block;
-  margin-right:3px;
+  color:#035ac0;
 }
 
+.base_wrapper article ul.list_decimal{
+  margin-bottom:35px;
+}
 .base_wrapper article ul.list_decimal li:before{
   content:"・";
   display: inline-block;
   margin-right:3px;
+}
+.base_wrapper article ul.list_date{
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin:10px -1% 15px;
+}
+.base_wrapper article ul.list_date li{
+  background:#009e96;
+  color:#fff;
+  padding:10px 15px;
+  width:48%;
+  text-align: center;
+  margin:0 1%;
+}
+
+@media(max-width:640px){
+  .base_wrapper article ul.list_date{
+    flex-direction: column;
+  }
+  .base_wrapper article ul.list_date li{
+    width:100%;
+  }
+  .base_wrapper article ul.list_date li:nth-of-type(1){
+    margin-bottom:10px;
+  }
+}
+
+.base_wrapper article ul.list_date li span{
+  margin-right:10px;
+  font-size:16px;
+}
+.base_wrapper article ul.list_date li strong{
+  font-size:20px;
+  font-weight:600;
 }
 
 .base_wrapper .pict_wrap{
@@ -169,11 +228,11 @@
   justify-content: center;
   flex-direction: column;
   color:#fff !important;
-  border-radius: 100px;
+  border-radius: 5px;
   font-size:20px;
   font-weight:bold;
-  background:#FCB400;
-  padding:10px 40px;
+  background:#009e96;
+  padding:20px 40px;
   text-align: center;
 }
 .btn_regs a:hover{
@@ -187,8 +246,15 @@
   }
 }
 
+#CONTENTS_TITLE{
+  background:url(/uploads/files/images/seminor/03/pub-seminor03-kv.jpg) no-repeat center center / cover;
+}
 #CONTENTS_TITLE .base_wrapper{
   text-align: center;
+}
+#CONTENTS_TITLE .base_wrapper h2,#CONTENTS_TITLE .base_wrapper h2 strong,
+#CONTENTS_TITLE .base_wrapper h3,#CONTENTS_TITLE .base_wrapper p{
+  color:#fff;
 }
 #CONTENTS_TITLE .base_wrapper h2,#CONTENTS_TITLE .base_wrapper h2 strong{
   display: flex;
@@ -208,8 +274,9 @@
 #CONTENTS_TITLE .base_wrapper h3{
   font-size:20px;
   margin-bottom:15px;
+  padding-left:0;
+  border:none;
 }
-
 @media(max-width:640px){
   #CONTENTS_TITLE .base_wrapper h2 strong span:first-of-type{
     font-size:20px;
@@ -229,7 +296,18 @@
 
 #CONTENTS_CATCH h3{
   text-align: center;
+  color:#035ac0;
+  padding-left:0;
+  border:none;
 }
+#CONTENTS_CATCH h3 strong{
+  font-size:40px;
+}
+#CONTENTS_CATCH h4{
+  font-size:24px;
+  text-align: center;
+}
+
 @media(max-width:640px){
   #CONTENTS_CATCH h3{
     font-size:20px;
@@ -239,6 +317,107 @@
   #CONTENTS_CATCH h3{
     font-size:18px;
   }
+}
+
+.point_wrap{
+  color:#fff;
+  text-align: center;
+  background:#5f52a0;
+  padding:35px;
+  margin-bottom:45px;
+}
+.point_wrap p{
+  font-size:18px;
+  line-height:1.8;
+  margin-bottom:0;
+}
+.point_wrap strong{
+  font-weight:bold;
+}
+
+#CATCH_IMAGES{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom:35px;
+}
+#CATCH_IMAGES .pict_wrap{
+  width:40%;
+  margin-right:15px;
+}
+@media(max-width:640px){
+  #CATCH_IMAGES{
+    flex-direction: column;
+  }
+  #CATCH_IMAGES .pict_wrap{
+    width:100%;
+    margin-right:0;
+    margin-bottom:15px;
+  }
+}
+
+#CONTENTS_TOKUTEN article{
+  display:flex;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+}
+#CONTENTS_TOKUTEN article .pict_wrap{
+  width: 45%;
+}
+@media(max-width:640px){
+  #CONTENTS_TOKUTEN article{
+    flex-direction: column;
+  }
+  #CONTENTS_TOKUTEN article .pict_wrap{
+    width: 100%;
+  }
+}
+
+#KOUENSHA_WRAP{
+  display:flex;
+  justify-content: space-between;
+}
+.kouensha_unit{
+  width:48%;
+}
+.kouensha_unit span,
+.kouensha_unit strong{
+  display:block;
+}
+.kouensha_unit strong{
+  font-size:24px;
+  font-weight:normal;
+}
+.kouensha_unit strong span{
+  display:inline-block;
+  margin-left:5px;
+  font-size:14px;
+}
+@media(max-width:640px){
+  #KOUENSHA_WRAP{
+    flex-direction: column;
+  }
+  .kouensha_unit{
+    width:100%;
+    margin-bottom:30px;
+  }
+}
+
+#CONTENTS_FLOW{
+
+}
+#CONTENTS_FLOW ol{
+  background:#eee;
+  padding:30px 5%;
+  margin-bottom:35px;
+}
+#CONTENTS_FLOW ol li span:nth-of-type(1){
+  display: inline-block;
+  background:#333;
+  color:#fff;
+  margin-right:10px;
+  padding:5px 10px;
+  line-height: 1;
+  border-radius: 3px;
 }
 
 </style>
