80d64d552cb94250793a8bd97a743e508a4bceb4
xxsyam <xxsyam@gmail.com>
2020-06-23 12:40:02 +0900

    20200623作業前

--- 7hj/mokuhyo/index.php
+++ 7hj/mokuhyo/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>
 <html lang="ja"
@@ -11,10 +10,10 @@
 	prefix="og: https://ogp.me/ns#"  class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!-->
 <html lang="ja"
-	prefix="og: https://ogp.me/ns#"  class="no-js"><!--<![endif]-->
+	prefix="og: https://ogp.me/ns#"  class="no-js">
+<!--<![endif]-->
 <head prefix="og: http://ogp.me/ns#">
 <meta charset="UTF-8">
-
 <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="format-detection" content="telephone=no">
@@ -29,135 +28,168 @@
 </head>
 
 <body class="lp">
-  <div id="kv-field"><a href="/class/" class="btn_parts type_a">近くの教室を探す</a></div>
-
-  <main>
-    <section id="about">
-      <h2><span>本プログラムは</span><strong>「成功する勉強方法を学び体験する」</strong><span>ための4日間の特別授業プログラムです。</span></h2>
-      <ul class="flex_wrapper center three">
-        <li><dl><dt>INPUT</dt><dd>マンガ<br>「ドラゴン桜」を読む</dd></dl></li>
-        <li><dl><dt>THINK</dt><dd>成功する<br>勉強法を学ぶ</dd></dl></li>
-        <li><dl><dt>TRY</dt><dd>自分が<br>何をするか決める</dd></dl></li>
-      </ul>
-      <p>本プログラムは、マンガ「ドラゴン桜」を読み（INPUT）、<br class="pc_display">
-        「成功哲学の名著」「不朽のバイブル」と言われる『7つの習慣』を通じ、<br class="pc_display">
-        成功する勉強法を学び（THINK）、自分が「何をするかを自分で考えて実践する」（TRY）特別授業プログラムです。<br>
-        なりたい自分になるために必要な「成功する勉強方法」を体験します。</p>
-    </section>
-
-    <section id="method">
+<div id="kv-field"><a href="/class/" class="btn_parts type_a">近くの教室を探す</a></div>
+<main>
+  <section id="about">
+    <h2><span>本プログラムは</span><strong>「成功する勉強方法を学び体験する」</strong><span>ための4日間の特別授業プログラムです。</span></h2>
+    <ul class="flex_wrapper center three">
+      <li>
+        <dl>
+          <dt>INPUT</dt>
+          <dd>マンガ<br>
+            「ドラゴン桜」を読む</dd>
+        </dl>
+      </li>
+      <li>
+        <dl>
+          <dt>THINK</dt>
+          <dd>成功する<br>
+            勉強法を学ぶ</dd>
+        </dl>
+      </li>
+      <li>
+        <dl>
+          <dt>TRY</dt>
+          <dd>自分が<br>
+            何をするか決める</dd>
+        </dl>
+      </li>
+    </ul>
+    <p>本プログラムは、マンガ「ドラゴン桜」を読み（INPUT）、<br class="pc_display">
+      「成功哲学の名著」「不朽のバイブル」と言われる『7つの習慣』を通じ、<br class="pc_display">
+      成功する勉強法を学び（THINK）、自分が「何をするかを自分で考えて実践する」（TRY）特別授業プログラムです。<br>
+      なりたい自分になるために必要な「成功する勉強方法」を体験します。</p>
+  </section>
+  <section id="method">
+    <h2><span>不朽の人生バイブル×東大合格勉強メソッド</span><strong>「なりたい自分になる」</strong><span>目標達成の原理原則を体験するプログラム</span></h2>
+    <div class="method_wrapper flex_wrapper center two">
+      <div class="method_wrap">
+        <h3><img src="./image/logo-7h.svg"><svg xmlns="https://www.7hj.jp/mokuhyo/image/logo-7h.svg" width="0">
+</svg><span>とは</span></h3>
+        <div class="pict_wrap"><img src="./image/pict-7hj.png"></div>
+        <div class="text_wrap">
+          <p>『7つの習慣』は全世界で4000万部、日本でも240万部売れた「不朽のバイブル」とも言われる世界的ベストセラーです。アメリカのスティーブン・R・コヴィー博士が、世界中の著名人や歴史的な偉人の生き方や考え方、行動などを検証し、彼らに共通する習慣を7つにまとめたものです。<br>
+            その内容は企業研修としても体系化され、今や米国フォーチュン100社の90%、グローバル・フォーチュン500社の75%に導入され、世界147カ国にて展開されています。また学校教育においても、アメリカなど5カ国で4000を超える学校にて導入されています。</p>
+        </div>
+      </div>
+      <div class="method_wrap">
+        <h3><img src="./image/logo-dragon.svg"><svg xmlns="https://www.7hj.jp/mokuhyo/image/logo-dragon.svg" width="0"><span>とは</span></h3>
+        <div class="pict_wrap"><img src="./image/pict-dragon.png"></div>
+        <div class="text_wrap">
+          <p>『ドラゴン桜』は、累計発行部数800万部を誇る三田紀房さん作の漫画です。弁護士・桜木建二が、平均偏差値36の学校の生徒を1年で東大合格に導くサクセスストーリーが描かれ、平成17年度文化庁メディア芸術祭マンガ部門優秀賞受賞。</p>
+          <p>2018年からは『ドラゴン桜2』が連載開始し、今回は、偏差値50の生徒が東大を目指している。受験テクニック・勉強法だけではなく、その教育法や、デジタル時代だからこその斬新かつ、本質的な「学び方」も話題に。2005年にTBSにてドラマ化し、2021年にも続編が公開予定！</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="about_7hj">
+    <h2><img src="./image/logo-7hj.svg"><span>とは</span></h2>
+    <div class="text_wrap">
+      <p>「すべての子どもたちをアクティブラーナー（能動的学修者）に」<br class="pc_display">
+        主体性を引き出し、課題発見能力、課題解決能力を育む<br class="pc_display">
+        アクティブラーナー育成授業</p>
+      <p>アクティブラーナ―（能動的学修者）とは自ら考え、学び、行動し、様々な問題を解決していく人を指します。<br class="pc_display">
+        2020年から全面実施となる学習指導要領でも、アクティブラーナーの育成に舵がきられることになりました。</p>
+      <p>世の中はものすごいスピードで変わっていきます。<br class="pc_display">
+        そうした世の中においては、誰も正解は分かりません。</p>
+      <p>今までのように<br class="pc_display">
+        「誰かが指示をしてくれるのを待つ」「言われた通りに行動する」「指示されないと行動できない」<br class="pc_display">
+        ではついていけないのです。</p>
+      <p>一人ひとりが自分の頭で考え、学び、行動し、問題を解決していくことが重要になる時代。<br class="pc_display">
+        子どもたちが今からそういった力を身につけ、未来においても自分の力を発揮しキラキラと笑顔で生きる。<br class="pc_display">
+        そんなアクティブラーナーになるための授業プログラムが「7つの習慣J®」です。</p>
+      <p>現在は、日本全国の中学校・高校、専門学校、短大・大学、学習塾を通じて<br class="pc_display">
+        年間約2万人もの生徒たちが本プログラムを受講しています。</p>
+    </div>
+  </section>
+  <section id="programs">
+    <div id="programs_head">
       <h2><span>不朽の人生バイブル×東大合格勉強メソッド</span><strong>「なりたい自分になる」</strong><span>目標達成の原理原則を体験するプログラム</span></h2>
-      <div class="method_wrapper flex_wrapper center two">
-        <div class="method_wrap">
-          <h3><img src="./image/logo-7hj.png"><span>とは</span></h3>
-          <div class="pict_wrap"><img src="./image/pict-7hj.png"></div>
-          <div class="text_wrap">
-            <p>『7つの習慣』は全世界で4000万部、日本でも240万部売れた「不朽のバイブル」とも言われる世界的ベストセラーです。アメリカのスティーブン・R・コヴィー博士が、世界中の著名人や歴史的な偉人の生き方や考え方、行動などを検証し、彼らに共通する習慣を7つにまとめたものです。</p>
-            <p>その内容は企業研修としても体系化され、今や米国フォーチュン100社の90%、グローバル・フォーチュン500社の75%に導入され、世界147カ国にて展開されています。また学校教育においても、アメリカなど5カ国で4000を超える学校にて導入されています。</p>
-          </div>
-        </div>
-        <div class="method_wrap">
-          <h3><img src="./image/logo-dragon.png"><span>とは</span></h3>
-          <div class="pict_wrap"><img src="./image/pict-dragon.png"></div>
-          <div class="text_wrap">
-            <p>『ドラゴン桜』は、累計発行部数800万部を誇る、三田紀房さん作の漫画です。弁護士・桜木建二が、平均偏差値36の学校の生徒を1年で東大合格に導くサクセスストーリーが描かれています。<br>
-              2003年から2007年まで、講談社の漫画雑誌『モーニング』にて連載され、平成17年度文化庁メディア芸術祭マンガ部門優秀賞受賞。2018年からは『ドラゴン桜2』も連載開始されました。<br>多くの受験テクニックや勉強法を紹介するとともに、その教育法や斬新な「学び方」も話題に。2005年にTBSにてドラマ化し、2020年にはドラマ化続編が公開予定！</p>
-          </div>
-        </div>
+    </div>
+    <div id="programs_wrapper" class="flex_wrapper two">
+      <div class="programs_wrap">
+        <h3><strong>「目標と計画の立て方」</strong>を学ぶ</h3>
+        <div class="pict_wrap"><img src="./image/programs_01.png"></div>
+        <div class="text_wrap">「なりたい自分」になるために、<br class="pc_display">
+          まずもっとも重要なことを学びます。</div>
       </div>
-    </section>
-
-    <section id="about_7hj">
-      <h2><img src="./image/logo-7hj_al.png"><span>とは</span></h2>
+      <div class="programs_wrap">
+        <h3><strong>「振り返り」</strong>を学ぶ</h3>
+        <div class="pict_wrap"><img src="./image/programs_02.png"></div>
+        <div class="text_wrap">自分の行動を正しく「振り返る」ことから<br class="pc_display">
+          得られる、気づきを未来に活かします。</div>
+      </div>
+      <div class="programs_wrap">
+        <h3><strong>「セルフマネジメント」</strong>を学ぶ</h3>
+        <div class="pict_wrap"><img src="./image/programs_03.png"></div>
+        <div class="text_wrap">思い通りにならないことを、<br class="pc_display">
+          自分の力で乗り越えるために重要なことを学びます。</div>
+      </div>
+      <div class="programs_wrap">
+        <h3><strong>「仲間と学ぶ方法」</strong>を学ぶ</h3>
+        <div class="pict_wrap"><img src="./image/programs_04.png"></div>
+        <div class="text_wrap">自分の努力を、仲間とともに<br class="pc_display">
+          さらに大きな力に変える方法、考え方を学びます。</div>
+      </div>
+    </div>
+  </section>
+  <section id="manga_field">
+    <h2><strong>マンガで学ぶ！実践して、体験する！</strong><span>桜木先生からのメッセージを読み解こう。</span></h2>
+    <p>授業でマンガを読み、気づきを振り返る。<br>
+      先生や塾の友達とシェアしながら進めます。<br>
+      そして具体的に「なりたい自分になる」行動計画をたて、実行しよう。</p>
+    <div class="pict_wrap"><img src="./image/pict_manga.png"></div>
+    <ul>
+      <li><span>マンガを読む</span></li>
+      <li><span>シェアして学ぶ</span></li>
+      <li><span>実践して体験する</span></li>
+    </ul>
+  </section>
+  <section id="games_field">
+    <div class="games_wrapper">
+      <div>
+        <h2><strong>「これからの学び方」を体験しよう！</strong><span>桜木先生、そして新たな仲間と学ぶ4日間</span></h2>
+        <div class="text_wrap">君の「目標達成」に向けて、<br class="pc_display">
+          俺も伴走するぞ！<br>
+          新しい自分にチャレンジする4日間。<br>
+          君の参加をまっている！</div>
+      </div>
+    </div>
+  </section>
+  <section id="search_school" class="lp-column white-type bg-type3">
+    <div class="inner-s flex_wrap">
       <div class="text_wrap">
-        <p>「すべての子どもたちをアクティブラーナー（能動的学修者）に」<br class="pc_display">
-          主体性を引き出し、課題発見能力、課題解決能力を育む<br class="pc_display">
-          アクティブラーナー育成授業</p>
-        <p>アクティブラーナ―（能動的学修者）とは自ら考え、学び、行動し、様々な問題を解決していく人を指します。<br class="pc_display">
-          2020年から全面実施となる学習指導要領でも、アクティブラーナーの育成に舵がきられることになりました。</p>
-        <p>世の中はものすごいスピードで変わっていきます。<br class="pc_display">
-          そうした世の中においては、誰も正解は分かりません。</p>
-        <p>今までのように<br class="pc_display">
-          「誰かが指示をしてくれるのを待つ」「言われた通りに行動する」「指示されないと行動できない」<br class="pc_display">
-          ではついていけないのです。</p>
-        <p>一人ひとりが自分の頭で考え、学び、行動し、問題を解決していくことが重要になる時代。<br class="pc_display">
-          子どもたちが今からそういった力を身につけ、未来においても自分の力を発揮しキラキラと笑顔で生きる。<br class="pc_display">
-          そんなアクティブラーナーになるための授業プログラムが「7つの習慣J®」です。</p>
-        <p>現在は、日本全国の中学校・高校、専門学校、短大・大学、学習塾を通じて<br class="pc_display">
-          年間約2万人もの生徒たちが本プログラムを受講しています。</p>
+        <h2>SEARCH</h2>
+        <h3>お近くの教室を探す</h3>
+        <p>「7つの習慣Ｊ<sub>&reg;</sub>」を体験してみませんか？<br />
+          まずは資料請求・無料体験にお問い合わせください。</p>
+		  <p><small>未実施校もあるので<br class="is-sp">詳細はお近くの校舎にお問合せください。</small></p>
+        <div class="btns center_btn"><a href="/class/" class="btn_parts type_a">近くの教室を探す</a></div>
       </div>
-    </section>
-
-    <section id="programs">
-      <div id="programs_head">
-        <h2><span>不朽の人生バイブル×東大合格勉強メソッド</span><strong>「なりたい自分になる」</strong><span>目標達成の原理原則を体験するプログラム</span></h2>
-      </div>
-
-      <div id="programs_wrapper" class="flex_wrapper two">
-        <div class="programs_wrap">
-          <h3><strong>「目標と計画の立て方」</strong>を学ぶ</h3>
-          <div class="pict_wrap"><img src="./image/programs_01.png"></div>
-          <div class="text_wrap">「なりたい自分」になるために、<br class="pc_display">まずもっとも重要なことを学びます。</div>
-        </div>
-        <div class="programs_wrap">
-          <h3><strong>「振り返り」</strong>を学ぶ</h3>
-          <div class="pict_wrap"><img src="./image/programs_02.png"></div>
-          <div class="text_wrap">自分の行動を正しく「振り返る」ことから<br class="pc_display">得られる、気づきを未来に活かします。</div>
-        </div>
-        <div class="programs_wrap">
-          <h3><strong>「セルフマネジメント」</strong>を学ぶ</h3>
-          <div class="pict_wrap"><img src="./image/programs_03.png"></div>
-          <div class="text_wrap">思い通りにならないことを、<br class="pc_display">自分の力で乗り越えるために重要なことを学びます。</div>
-        </div>
-        <div class="programs_wrap">
-          <h3><strong>「仲間と学ぶ方法」</strong>を学ぶ</h3>
-          <div class="pict_wrap"><img src="./image/programs_04.png"></div>
-          <div class="text_wrap">自分の努力を、仲間とともに<br class="pc_display">さらに大きな力に変える方法、考え方を学びます。</div>
-        </div>
-      </div>
-    </section>
-
-    <section id="manga_field">
-      <h2><strong>マンガで学ぶ！実践して、体験する！</strong><span>桜木先生からのメッセージを読み解こう。</span></h2>
-      <p>授業でマンガを読み、気づきを振り返る。<br>
-        先生や塾の友達とシェアしながら進めます。<br>
-        そして具体的に「なりたい自分になる」行動計画をたて、実行しよう。</p>
-      <div class="pict_wrap"><img src="./image/pict_manga.png"></div>
-
-      <ul>
-        <li><span>マンガを読む</span></li>
-        <li><span>シェアして学ぶ</span></li>
-        <li><span>実践して体験する</span></li>
-      </ul>
-    </section>
-
-    <section id="games_field">
-      <div class="games_wrapper">
-        <div>
-          <h2><strong>「これからの学び方」を体験しよう！</strong><span>桜木先生、そして新たな仲間と学ぶ4日間</span></h2>
-          <div class="text_wrap">君の「目標達成」に向けて、<br class="pc_display">俺も伴走するぞ！<br>新しい自分にチャレンジする4日間。<br>君の参加をまっている！</div>
-        </div>
-      </div>
-    </section>
-
-    <section id="contact">
-      <div class="contact_wrap">
+      <div class="pict_wrap"><img src="//www.7hj.jp/wp-content/themes/7hj/images/search_school.png" /></div>
+    </div>
+  </section>
+  <section id="contact"> 
+    <!-- <div class="contact_wrap">
         <h2><strong>お問い合わせ</strong><span>近くの教室へまずは問い合わせだ！</span></h2>
         <a href="/class/" class="btn_parts type_a">近くの教室を探す</a>
-      </div>
-
-			<div class="contact_wrap">
-        <h2><strong>プログラムの導入をお考えの方へ</strong></h2>
-				<p>「7つの習慣Ｊ<sub>&reg;</sub>」プログラムは、全国の多くの教育機関や学習塾において、<br>子どもたちの総合的な成長を支える特色あるプログラムとしてご活用いただいています。</p>
-        <a href="/introduction/" class="btn_parts type_a">プログラム導入について</a>
-      </div>
-    </section>
-  </main>
-
-  <footer>
-    <div><small>Copyright &copy; FCE Education Inc. All Rights Reserved.</small></div>
-    <div><ul><li><a href="/privacy-policy/">プライバシーポリシー</a></li><li><a href="/corporate/">会社概要</a></li></ul><small><span>&copy;三田紀房/コルク</span>　<span>「7つの習慣<sub>&reg;</sub>」および「7つの習慣J<sub>&reg;</sub>」はフランクリン・コヴィー・ジャパン社の登録商標です。</span></small></div>
-  </footer>
-
+      </div> -->
+    
+    <div class="contact_wrap">
+      <h2>プログラムの導入をお考えの方へ</h2>
+      <p>「7つの習慣Ｊ<sub>&reg;</sub>」プログラムは、全国の多くの教育機関や学習塾において、<br>
+        子どもたちの総合的な成長を支える特色あるプログラムとしてご活用いただいています。</p>
+      <a href="/introduction/" class="btn_parts type_a">プログラム導入について</a> </div>
+  </section>
+</main>
+<footer>
+  <div><small>Copyright &copy; FCE Education Inc. All Rights Reserved.</small></div>
+  <div>
+    <ul>
+      <li><a href="/privacy-policy/">プライバシーポリシー</a></li>
+      <li><a href="/corporate/">会社概要</a></li>
+    </ul>
+    <small><span>&copy;三田紀房/コルク</span>　<span>「7つの習慣<sub>&reg;</sub>」および「7つの習慣J<sub>&reg;</sub>」はフランクリン・コヴィー・ジャパン社の登録商標です。</span></small></div>
+</footer>
 </body>
 </html>
