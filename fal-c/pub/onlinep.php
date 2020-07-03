a3ef18abe699d520c1437092c0746e442b9b6081
xxsyam <xxsyam@gmail.com>
2020-07-03 21:51:42 +0900

    20200703作業済

---
+++ fal-c/pub/onlinep.php
@@ -0,0 +1,469 @@
+<section class="base_section" id="CONTENTS_TITLE">
+  <div class="base_wrapper">
+    <div class="inner">
+      <h2><span>Find！アクティブラーナーの新サービス！</span><strong><img src="/uploads/files/images/onlinesalon/logo.png"></strong></h2>
+      <h3>プレミアムプラン＆ダイヤモンドプランがさらにお得に！</h3>
+      <p>この度、Find！アクティブラーナーは、<br>新しい学びの場として「Find！オンラインサロン」をスタートします！</p>
+
+      <ul class="pict_list">
+        <li><span></span><span>最先端の教育知識や<br>スキルを身につけたい</span></li>
+        <li><span></span><span>全国の教員と<br>学びを共有したい</span></li>
+        <li><span></span><span>新しい取り組みに<br>チャレンジしたい</span></li>
+      </ul>
+    </div>
+  </div>
+</section>
+
+<section class="base_section" id="CATCH_FIELD">
+  <div class="base_wrapper">
+    <div class="inner">
+      <h3><span>新しい学びの場として</span><strong>「Find！オンラインサロン」をスタート！</strong></h3>
+      <p>対象は、<strong>プレミアムプラン＆ダイヤモンドプラン会員限定</strong>のサービスとして、<br>特別招待させていただくFacebookグループ上でご提供させていただきます。</p>
+
+      <div class="detail_wrap">
+        <p>オンラインサロンでは、定期開催されるオンラインセミナーで登壇される講師と、直接質疑応答ができたり、弊社コンテンツを通じた会員間のコミュニケーション（階層別分科会・課題別分科会など予定）にご活用いただけます！<br>（さらに、プレミアムプラン＆ダイヤモンドプラン会員の方へは、有料オノンラインセミナーが特別無料でご参加いただける特典もついております。）</p>
+        <p>従来の教育だけでなくオンライン教育とのハイブリッドなど、より多くの知見やノウハウが求められるこれからの教育について、最先端の学びを有識者から、そして会員同士で学びを共有して深めていただけます！</p>
+        <p>この新しい学びの場で、より多くの子供達を笑顔にするための取り組みをご提供します。ぜひこの機会に、オンラインサロンへの参加（＋各種特典付き）を含んだプレミアムプラン＆ダイヤモンドプランをご利用ください！</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="SALONS_PARTICIPATION" class="base_section">
+  <div class="base_wrapper">
+    <div class="inner">
+      <h3>オンラインサロンへの参加方法</h3>
+      <h4>［事前準備］Facebookアカウントをご用意ください</h4>
+
+      <p>Find！オンラインサロンは、Facebookのサービスを利用して運営いたします。<br>
+        ※Facebookアカウントの登録には電話番号、もしくはメールアドレスが必要です。<br>
+        ※実名フルネームでのご登録をお願いしています。</p>
+
+      <dl>
+        <dt>1．以下フォームより、会員IDとFacebook上のご登録名をお伝えください</dt>
+        <dd><iframe src="https://ws.formzu.net/dist/S5051075/" style="height: 500px; width: 100%; border: 0;" onload="if (navigator.userAgent.match(/iPhone|iPad|Macintosh/) !== null && formzuInitialLoad == true) this.scrollIntoView(true)"></iframe>
+          <script>
+          if (window.addEventListener) {
+             window.addEventListener( 'load', formzuInitialSetting, false );
+          }
+          else if (window.attachEvent) {
+             window.attachEvent( 'onload', formzuInitialSetting );
+          }
+          else {
+             window.onload = formzuInitialSetting;
+          }
+          function formzuInitialSetting() {
+             formzuInitialLoad = true;
+          }
+          </script>
+        </dd>
+      </dl>
+
+      <dl>
+        <dt>２．以下Find！オンラインサロン（Facebookグループ）にアクセスし、参加申請を行ってください。</dt>
+        <dd>
+          <p>◇Find！オンラインサロン-限定公開グループ<br>
+            <a href="https://www.facebook.com/groups/260622201689730" target="_blank">https://www.facebook.com/groups/260622201689730</a>
+          </p>
+        </dd>
+      </dl>
+
+      <dl>
+        <dt>３．承認完了の連絡が来たら、入会完了！</dt>
+        <dd>Find！オンラインサロンに投稿されたコンテンツなどを自由に閲覧して楽しみましょう！また、会員間の交流なども進めてください。<br>
+          ※申請から承認まで最大で１週間ほどお時間をいただきます。<br>
+          ※リクエストが承認されると、Facebookから「Find！オンラインサロンへの参加リクエストが承認されました。」と通知が送られてきます。<br>
+          ※承認の連絡がない場合も、サロンを閲覧できていれば承認は完了しています。 </dd>
+      </dl>
+
+    </div>
+  </div>
+</section>
+
+<section id="ENROLLMENT_ATTENTION" class="base_section">
+  <div class="base_wrapper">
+    <div class="inner">
+      <h3>ご入会に際しての諸注意</h3>
+      <dl>
+        <dt>注意事項</dt>
+        <dd>
+          <ul>
+            <li>こちらのオンラインサロンは事前入会制となります。</li>
+            <li>会員情報は、実名フルネームでご登録いただきますようお願いいたします。</li>
+            <li>内部の情報を漏らさず秘密を厳守できる方のみ入会できます。</li>
+            <li>オンラインサロン内外を問わず、運営および特定メンバーへの誹謗中傷、荒らし行為などコミュニティの秩序を乱す行為があった場合は強制退会とさせていただく場合があります。</li>
+          </ul>
+        </dd>
+      </dl>
+      <dl>
+        <dt>​退会について</dt>
+        <dd>
+          <ul>
+            <li>退会を希望される方は 以下よりご連絡ください。<br>
+              Find！アクティブラーナー事務局：info@find-activelearning.com </li>
+            <li>月末に退会希望をされた場合は翌月処理になることがございます。あらかじめご了承ください。</li>
+          </ul>
+        </dd>
+      </dl>
+    </div>
+  </div>
+</section>
+
+<style type="text/css">
+@charset "UTF-8";
+.base_section {
+	padding: 80px 5%;
+}
+
+@media (max-width: 640px) {
+	.base_section {
+		padding: 40px 5%;
+	}
+}
+
+.base_section * {
+	line-height: 1.8;
+}
+
+.base_section .base_wrapper {
+	max-width: 1100px;
+	margin: auto;
+}
+
+.base_section .base_wrapper .inner {
+	max-width: 1000px;
+	margin: auto;
+}
+
+.base_section .base_wrapper h3 {
+	text-align: center;
+	margin-bottom: 35px;
+	color: #0365bf;
+}
+
+.base_section .base_wrapper h3 span, .base_section .base_wrapper h3 strong {
+	display: block;
+}
+
+.base_section .base_wrapper h3 span {
+	font-size: 20px;
+}
+
+.base_section .base_wrapper h3 strong {
+	font-size: 32px;
+}
+
+.pict_list {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: space-between;
+	width: 100%;
+}
+
+@media (max-width: 640px) {
+	.pict_list {
+		flex-direction: column;
+	}
+}
+
+.pict_list li {
+	margin: 0 2%;
+}
+
+.pict_list li span {
+	display: block;
+	background-size: cover;
+}
+
+.pict_list li span:nth-of-type(1) {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	min-width: 300px;
+	min-height: 300px;
+	background: #ccc;
+	border-radius: 50%;
+	overflow: hidden;
+	margin-bottom: 15px;
+}
+
+.pict_list li span:nth-of-type(1) img {
+	margin: auto;
+	max-width: 100%;
+}
+
+.pict_list li span:nth-of-type(2) {
+	margin-top: 10px;
+	font-weight: 600;
+}
+
+@media (max-width: 640px) {
+	.pict_list li:not(:last-of-type) {
+		margin-bottom: 35px;
+	}
+}
+
+.line_list {
+	border-top: 1px solid #333;
+	border-bottom: 1px solid #333;
+	padding: 35px;
+}
+
+.line_list li {
+	line-height: 2;
+}
+
+.seminors_wrapper {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: space-between;
+	align-items: baseline;
+}
+
+@media (max-width: 640px) {
+	.seminors_wrapper {
+		flex-direction: column;
+	}
+}
+
+.seminors_wrapper .seminor_wrap {
+	width: 32%;
+}
+
+@media (max-width: 640px) {
+	.seminors_wrapper .seminor_wrap {
+		width: 100%;
+	}
+}
+
+.seminors_wrapper .seminor_wrap .text_wrap {
+	margin: 15px 0 0;
+	text-align: left;
+}
+
+@media (max-width: 640px) {
+	.seminors_wrapper .seminor_wrap .text_wrap {
+		margin-bottom: 35px;
+	}
+}
+
+.seminors_wrapper .seminor_wrap .text_wrap h4 {
+	font-size: 16px;
+	font-weight: 600;
+}
+
+.seminors_wrapper .seminor_wrap .text_wrap dl {
+	display: flex;
+}
+
+.seminors_wrapper .seminor_wrap .text_wrap dl dt {
+	white-space: nowrap;
+}
+
+#CONTENTS_TITLE {
+	padding: 45px 0;
+	min-height: 35vh;
+	background: #fff url(/uploads/files/images/onlinesalon/mv_bg.jpg) center center/cover;
+}
+
+#CONTENTS_TITLE .base_wrapper .inner {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: column;
+	flex-wrap: nowrap;
+}
+
+@media (max-width: 640px) {
+	#CONTENTS_TITLE .base_wrapper .inner img {
+		width: 100%;
+	}
+}
+
+#CONTENTS_TITLE .base_wrapper .pict_list li:nth-of-type(1) span:nth-of-type(1) {
+	background: url(/uploads/files/images/onlinesalon/pict_01.jpg) no-repeat 90% top;
+}
+
+#CONTENTS_TITLE .base_wrapper .pict_list li:nth-of-type(2) span:nth-of-type(1) {
+	background: url(/uploads/files/images/onlinesalon/pict_02.jpg) no-repeat 40% top;
+}
+
+#CONTENTS_TITLE .base_wrapper .pict_list li:nth-of-type(3) span:nth-of-type(1) {
+	background: url(/uploads/files/images/onlinesalon/pict_03.jpg) no-repeat 100% top;
+}
+
+#CONTENTS_TITLE .base_wrapper h2 span, #CONTENTS_TITLE .base_wrapper h2 strong {
+	display: block;
+}
+
+#CONTENTS_TITLE .base_wrapper h2 span {
+	font-size: 16px;
+	font-weight: 600;
+	padding-bottom: 15px;
+}
+
+#CONTENTS_TITLE .base_wrapper h2 strong {
+	margin-bottom: 15px;
+}
+
+#CONTENTS_TITLE .base_wrapper h2 strong img {
+	max-width: 560px;
+}
+
+#CONTENTS_TITLE .base_wrapper h3 {
+	font-size: 18px;
+	font-weight: 600;
+	margin-bottom: 55px;
+}
+
+#CONTENTS_TITLE .base_wrapper p {
+	margin-bottom: 45px;
+}
+
+#CATCH_FIELD .inner {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: column;
+	flex-wrap: nowrap;
+}
+
+#CATCH_FIELD .inner > p {
+	display: inline-block;
+	text-align: left;
+	margin-bottom: 45px;
+}
+
+#CATCH_FIELD .inner .detail_wrap {
+	border: 1px solid #0365bf;
+	padding: 35px;
+	text-align: left;
+}
+
+#TOKUTEN_FIELD {
+	background: #fff url(/uploads/files/images/onlinesalon/tokuten_bg.jpg) center center/cover;
+}
+
+#TOKUTEN_FIELD .inner .pict_list:nth-of-type(1) li span:nth-of-type(1) {
+	background-color: #0365bf;
+}
+
+#TOKUTEN_FIELD .inner .pict_list:nth-of-type(2) {
+	margin-bottom: 45px;
+}
+
+#TOKUTEN_FIELD .inner .pict_list:nth-of-type(2) li span:nth-of-type(1) {
+	background-color: #fff;
+}
+
+#TOKUTEN_FIELD .inner h4 {
+	text-align: center;
+	margin: 35px;
+	font-size: 20px;
+}
+
+#NEXT_SEMINORS .inner {
+	margin-top: 45px;
+	border: 1px solid #0365bf;
+	padding: 35px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: column;
+	flex-wrap: nowrap;
+}
+
+#NEXT_SEMINORS .inner .btn_add {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	margin-top: 45px;
+}
+
+#NEXT_SEMINORS .inner .btn_add a {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	text-align: center;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	padding: 10px 40px;
+	color: #0365bf;
+	font-size: 20px;
+	font-weight: 600;
+	background: #fff45c;
+	border-radius: 100px;
+}
+
+#NEXT_SEMINORS .inner .btn_add a:hover {
+	opacity: 0.75;
+}
+
+#SALONS_PARTICIPATION {
+	background: #fff url(/uploads/files/images/onlinesalon/mv_bg.jpg) center center/cover;
+}
+
+#SALONS_PARTICIPATION h4, #SALONS_PARTICIPATION p {
+	text-align: center;
+	margin-bottom: 35px;
+}
+
+#SALONS_PARTICIPATION h4 {
+	color: #ff0909;
+	font-size: 20px;
+}
+
+#SALONS_PARTICIPATION dl, #ENROLLMENT_ATTENTION dl {
+	margin-bottom: 45px;
+}
+
+#SALONS_PARTICIPATION dl dt, #ENROLLMENT_ATTENTION dl dt {
+	display: inline-block;
+	color: #0365bf;
+	margin-bottom: 15px;
+	font-size: 20px;
+	font-weight: 600;
+	border-bottom: 1px solid #0365bf;
+}
+
+#SALONS_PARTICIPATION dl dd ul li:before, #ENROLLMENT_ATTENTION dl dd ul li:before {
+	content: "・";
+	display: inline-block;
+	margin-right: 5px;
+}
+
+#SALONS_PARTICIPATION dl dd p, #ENROLLMENT_ATTENTION dl dd p {
+	text-align: left;
+}
+
+#SALONS_PARTICIPATION dl dd p a, #ENROLLMENT_ATTENTION dl dd p a {
+	color: #0365bf;
+}
+
+/*# sourceMappingURL=add.css.map */
+</style>
---
+++ fal-c/pub/seminor20718p.php
@@ -0,0 +1,501 @@
+<section class="base_section" id="CONTENTS_TITLE">
+<div class="base_wrapper">
+<h2><span>新しい学びの形「Find！オンラインサロン」</span><strong><span>開設記念オンラインセミナー</span><span>第1弾<br>オンラインでアクティブ・ラーニング実践編</span></strong></h2>
+
+<h3>7月18日（土）13:30～15:30　開催決定！</h3>
+
+<p>＜セミナーお申し込みはページ下部記載＞</p>
+</div>
+</section>
+
+<section class="base_section" id="CONTENTS_CATCH">
+<div class="base_wrapper">
+<h3>4日間で<strong>1,400</strong>名超が申し込んだオンラインセミナー<br>
+「オンライン授業導入＜基礎研修編＞」の“応用編”を開催！</h3>
+
+<div class="point_wraps">
+<p>この度、Find！アクティブラーナーは、先生方の新しい学びの場として「Find！オンラインサロン」を開設いたします。<br>
+一部会員（プレミアムorダイヤモンドプラン会員）を対象とした限定のFacebookグループにて、弊社コンテンツを通じたメンバー間のコミュニケーションや、定期開催するオンラインセミナーの質疑応答等を行う場として、より学びの深まる機会を提供すべくご用意いたしました。</p>
+
+<div class="pict_wrap"><img src="/uploads/files/images/seminor/20718/online.png"></div>
+
+<p>今回は、その「Find！オンラインサロン」を皆様にご利用いただく機会も兼ね、開設記念のオンラインセミナー「オンラインでアクティブ・ラーニング実践編」を開催いたします。<br>
+前回開催にて頂いた「オンライン授業でもアクティブ・ラーニングはできるのか」「応用編を知りたい」というご要望にお応えする内容となりますので、ぜひこの機会にご参加ください。</p>
+</div>
+
+</section>
+
+<section class="base_section" id="CONTENTS_GAIYO">
+<div class="base_wrapper">
+<h3>オンラインセミナー概要</h3>
+
+<article>
+<h4>日時</h4>
+
+<ul class="list_date">
+<li><span>2020年</span><strong>7月18日(土) 13:30～15:30</strong></li>
+</ul>
+
+</article>
+
+<article>
+<h4>参加費</h4>
+
+<p>2,200円(税込)<br>
+※Find！アクティブラーナー個人プレミアムorダイヤモンドプラン会員の方は“無料”でご参加いただけます<br>
+※Find！アクティブラーナー個人文字コンテンツプラン会員の方は、以下URL（フォーム）よりお手続きをお願いいたします</p>
+
+</article>
+
+<article>
+<h4>当日のセミナー内容（予定）</h4>
+
+<ul class="list_decimal">
+<li>オンラインセミナーの全体像、目的の共有</li>
+<li>アクティブ・ラーニング技法を活用した模擬授業の展開</li>
+<li>フリップメソッド</li>
+<li>Chat、投票機能を活用した授業展開</li>
+<li>ピアインストラクション</li>
+<li>Learning in Teachingのペアワーク</li>
+<li>グループワークを活用したAL</li>
+<li>オンライン・ファシリテーションなどの技法を取り混ぜた模擬授業の展開</li>
+</ul>
+
+<p>当日は「Zoom」を使用し、実施いたします<br />
+※古いバージョンの「Zoom」をお使いの場合、画面や資料が表示されないなどの不具合が生じる可能性ございますので、事前に最新バージョンへのアップデートをお願いいたします</p>
+</article>
+</div>
+</section>
+
+<section class="base_section" id="CONTENTS_TOKUTEN">
+<div class="base_wrapper">
+<h3>Find!オンラインサロン利用者限定特典</h3>
+
+
+<div id="CATCH_IMAGES">
+  <div class="pict_wrap"><img src="/uploads/files/images/seminor/20718/pub_seminor_00.jpg"></div>
+  <div class="text_wrap">
+    <p>セミナー終了後2週間（本開催分は2020年7月31日（金）まで）は、Find！オンラインサロン（Facebookグループ）内のスレッド（分科会）にて、参加者の皆様から頂きました質問に対して、講師が順に回答していきます。<br><br>
+    講師とのコミュニケーション、質疑応答を通じて、より理解を深めたいという方は、ぜひオンラインサロンにご参加ください。</p>
+  </div>
+</div>
+
+
+</div>
+</section>
+
+<section class="base_section" id="CONTENTS_KOUENSHA">
+<div class="base_wrapper">
+<h3>講演者紹介</h3>
+
+<div id="KOUENSHA_WRAP">
+<div class="kouensha_unit">
+<h4>メイン講師</h4>
+<div class="pict_wrap"><img src="/uploads/files/images/seminor/teachers/teraura.jpg" /></div>
+
+<p><span>株式会社学び代表取締役<br />
+一般社団法人アクティブ・ラーニング協会理事</span><strong>寺裏　誠司<span>氏</span></strong></p>
+</div>
+
+<div class="kouensha_unit">
+<h4>モデレーター</h4>
+<div class="pict_wrap"><img src="/uploads/files/images/seminor/teachers/nakajima.jpg" /></div>
+
+<p><span>元全国高等学校長協会教育課程研究委員長<br />
+前茨城県立並木中等教育学校校長<br />
+株式会社Findアクティブラーナー参与<br />
+アクティブ・ラーニング協会代表理事</span><strong>中島　博司<span>氏</span></strong></p>
+</div>
+</div>
+</div>
+</section>
+<section class="base_section" id="CONTENTS_FLOW">
+<div class="base_wrapper">
+<h3>お申込みの流れ</h3>
+
+<h4 class="line mb40px">以下フォームに必要事項をご記入の上、お申し込みください</h4>
+
+<iframe src="https://ws.formzu.net/dist/S65832451/" style="height: 600px; width: 100%; border: 0;" onload="if (navigator.userAgent.match(/iPhone|iPad|Macintosh/) !== null && formzuInitialLoad == true) this.scrollIntoView(true)"></iframe>
+<script>
+if (window.addEventListener) {
+   window.addEventListener( 'load', formzuInitialSetting, false );
+}
+else if (window.attachEvent) {
+   window.attachEvent( 'onload', formzuInitialSetting );
+}
+else {
+   window.onload = formzuInitialSetting;
+}
+function formzuInitialSetting() {
+   formzuInitialLoad = true;
+}
+</script>
+
+<p>※セミナー開催日の３日前正午迄に、当日のオンラインセミナー招待URLをメールでお届けします。<br>
+メールを確認できない場合は、以下へお問い合わせください<br>
+Find！アクティブラーナー事務局：info@find-activelearning.com<br>
+（迷惑メールフォルダに振り分けられている場合もございますので、お手数ですがそちらのご確認もお願いいたします）</p>
+</div>
+</section>
+
+<section class="base_section" id="JIMUKYOKU_FIELD">
+<div class="base_wrapper">
+<h4>お問い合わせ先</h4>
+<p>Find！アクティブラーナー事務局<br>
+<a href="mailto:info@find-activelearning.com">info@find-activelearning.com</a></p>
+</div>
+</section>
+
+<style type="text/css">
+.base_section{
+  padding:80px 5%;
+}
+.base_section a{
+  color:#045ac0;
+}
+@media(max-width:640px){
+  .base_section{
+    padding:40px 5%;
+  }
+}
+.base_section *{
+  line-height:1.8;
+}
+.base_section:nth-of-type(2n+1){
+  background:#fffce4;
+}
+
+.base_wrapper{
+  max-width:900px;
+  margin:auto;
+}
+.base_wrapper p{
+  margin-bottom:15px;
+}
+.base_wrapper h3{
+  color:#045ac0;
+  border-left:10px solid #045ac0;
+  padding-left:15px;
+}
+
+.base_wrapper h3,.base_wrapper article:not(:last-of-type){
+  margin-bottom:35px;
+}
+.base_wrapper h4{
+  color:#035ac0;
+}
+.base_wrapper h4.line{
+  border-bottom:2px solid #035ac0;
+  font-weight:600;
+}
+@media(max-width:640px){
+  .base_wrapper h4.line{
+    border-bottom:none;
+  }
+}
+.base_wrapper .mt40px{
+  margin-top:50px;
+}
+
+.base_wrapper article ul.list_decimal{
+  margin-bottom:35px;
+}
+.base_wrapper article ul.list_decimal li:before{
+  content:"・";
+  display: inline-block;
+  margin-right:3px;
+}
+.base_wrapper article ul.list_date{
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin:10px -1% 15px;
+}
+.base_wrapper article ul.list_date li{
+  background:#009e96;
+  color:#fff;
+  padding:10px 15px;
+  width:48%;
+  text-align: center;
+  margin:0 1%;
+}
+
+@media(max-width:640px){
+  .base_wrapper article ul.list_date{
+    flex-direction: column;
+  }
+  .base_wrapper article ul.list_date li{
+    width:100%;
+  }
+  .base_wrapper article ul.list_date li:nth-of-type(1){
+    margin-bottom:10px;
+  }
+}
+
+.base_wrapper article ul.list_date li span{
+  margin-right:10px;
+  font-size:16px;
+}
+.base_wrapper article ul.list_date li strong{
+  font-size:20px;
+  font-weight:600;
+}
+@media(max-width:640px){
+  .base_wrapper article ul.list_date li strong{
+    font-size:16px;
+  }
+}
+
+.base_wrapper .pict_wrap{
+  margin-bottom:15px;
+}
+.base_wrapper .pict_wrap img{
+  width:100%;
+}
+
+.base_wrapper ol li{
+  margin-bottom:15px;
+}
+.attention_text{
+  color:red;
+  display: block;
+}
+
+.btn_regs{
+  margin-top:10px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.btn_regs a{
+  max-width:340px;
+  margin:auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color:#fff !important;
+  border-radius: 5px;
+  font-size:20px;
+  font-weight:bold;
+  background:#009e96;
+  padding:20px 40px;
+  text-align: center;
+}
+.btn_regs a:hover{
+  opacity:0.75;
+}
+@media(max-width:480px){
+  .btn_regs a{
+    margin:0 5%;
+    font-size:16px;
+    padding:10px 20px;
+  }
+}
+
+#CONTENTS_TITLE{
+  background:url(/uploads/files/images/seminor/20718/pub-seminor-kv.jpg) no-repeat center center / cover;
+}
+#CONTENTS_TITLE .base_wrapper{
+  text-align: center;
+}
+#CONTENTS_TITLE .base_wrapper h2,#CONTENTS_TITLE .base_wrapper h2 strong,
+#CONTENTS_TITLE .base_wrapper h3,#CONTENTS_TITLE .base_wrapper p{
+  color:#fff;
+}
+#CONTENTS_TITLE .base_wrapper h2,#CONTENTS_TITLE .base_wrapper h2 strong{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+#CONTENTS_TITLE .base_wrapper h2>span{
+  font-size:16px;
+}
+#CONTENTS_TITLE .base_wrapper h2 strong span:first-of-type{
+  font-size:24px;
+}
+#CONTENTS_TITLE .base_wrapper h2 strong span:last-of-type{
+  font-size:32px;
+  margin-bottom:35px;
+}
+#CONTENTS_TITLE .base_wrapper h3{
+  font-size:20px;
+  margin-bottom:15px;
+  padding-left:0;
+  border:none;
+}
+@media(max-width:640px){
+  #CONTENTS_TITLE .base_wrapper h2 strong span:first-of-type{
+    font-size:20px;
+  }
+  #CONTENTS_TITLE .base_wrapper h2 strong span:last-of-type{
+    font-size:26px;
+  }
+  #CONTENTS_TITLE .base_wrapper h3{
+    font-size:18px;
+  }
+}
+@media(max-width:480px){
+  #CONTENTS_TITLE .base_wrapper h2 strong span:last-of-type{
+    font-size:22px;
+  }
+}
+
+#CONTENTS_CATCH h3{
+  text-align: center;
+  color:#035ac0;
+  padding-left:0;
+  border:none;
+}
+#CONTENTS_CATCH h3 strong{
+  font-size:40px;
+}
+#CONTENTS_CATCH h4{
+  font-size:24px;
+  text-align: center;
+}
+
+@media(max-width:640px){
+  #CONTENTS_CATCH h3{
+    font-size:20px;
+  }
+}
+@media(max-width:480px){
+  #CONTENTS_CATCH h3{
+    font-size:18px;
+  }
+}
+.point_wraps .pict_wrap{
+  margin:30px auto;
+}
+.point_wrap{
+  color:#fff;
+  text-align: left;
+  background:#5f52a0;
+  padding:35px;
+  margin-bottom:45px;
+}
+.point_wrap p{
+  font-size:18px;
+  line-height:1.8;
+  margin-bottom:0;
+}
+.point_wrap strong{
+  font-weight:bold;
+}
+
+#CATCH_IMAGES{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom:35px;
+}
+#CATCH_IMAGES .pict_wrap{
+  width:45%;
+  margin-right:35px;
+}
+#CATCH_IMAGES .text_wrap{
+  width:55%;
+}
+@media(max-width:640px){
+  #CATCH_IMAGES{
+    flex-direction: column;
+  }
+  #CATCH_IMAGES .pict_wrap{
+    width:100%;
+    margin-right:0;
+    margin-bottom:15px;
+  }
+  #CATCH_IMAGES .text_wrap{
+    width:100%;
+  }
+}
+
+#CONTENTS_TOKUTEN article{
+  display:flex;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+}
+#CONTENTS_TOKUTEN article .pict_wrap{
+  width: 45%;
+}
+@media(max-width:640px){
+  #CONTENTS_TOKUTEN article{
+    flex-direction: column;
+  }
+  #CONTENTS_TOKUTEN article .pict_wrap{
+    width: 100%;
+  }
+}
+
+#KOUENSHA_WRAP{
+  display:flex;
+  justify-content: space-between;
+}
+.kouensha_unit{
+  width:46%;
+}
+.kouensha_unit span,
+.kouensha_unit strong{
+  display:block;
+}
+.kouensha_unit strong{
+  font-size:24px;
+  font-weight:normal;
+}
+.kouensha_unit strong span{
+  display:inline-block;
+  margin-left:5px;
+  font-size:14px;
+}
+@media(max-width:640px){
+  #KOUENSHA_WRAP{
+    flex-direction: column;
+  }
+  .kouensha_unit{
+    width:100%;
+    margin-bottom:30px;
+  }
+}
+#CONTENTS_FLOW ol{
+  background:#eee;
+  padding:30px 5%;
+  margin-bottom:35px;
+}
+#CONTENTS_FLOW ol li span:nth-of-type(1){
+  display: inline-block;
+  background:#333;
+  color:#fff;
+  margin-right:10px;
+  padding:5px 10px;
+  line-height: 1;
+  border-radius: 3px;
+}
+.fred{
+  color:red;
+  font-weight:600;
+}
+
+#JIMUKYOKU_FIELD{
+  background:#fff;
+}
+#JIMUKYOKU_FIELD .base_wrapper{
+  display:flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  padding:45px;
+  margin:auto;
+  max-width:900px;
+  border:2px solid #035ac0;
+  text-align: center;
+}
+@media(max-width:640px){
+  #JIMUKYOKU_FIELD .base_wrapper{
+    padding:25px;
+  }
+}
+#JIMUKYOKU_FIELD h4{
+  color:#035ac0;
+  font-size:18px;
+}
+</style>
